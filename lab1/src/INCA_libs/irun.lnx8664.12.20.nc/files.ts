3c4164afc9fce9192bb4415e6a7a80b0f2e76dd9
DCLab_team02 <DCLab_team02>
2020-10-15 09:35:03 +0800

    lab2 tb pass

---
+++ lab1/src/INCA_libs/irun.lnx8664.12.20.nc/files.ts
@@ -0,0 +1,6 @@
+1600848096 /home/team02/lab1/src/DE2_115/DE2_115.sv
+1600609203 /home/team02/lab1/src/DE2_115/Debounce.sv
+1600609203 /home/team02/lab1/src/DE2_115/SevenHexDecoder.sv
+1600609203 /home/team02/lab1/src/Top_example.sv
+1600850131 /home/team02/lab1/src/Top.sv
+1600773052 /home/team02/lab1/src/tb.sv
---
+++ lab1/src/nWaveLog/nWave.cmd
@@ -0,0 +1,1 @@
+wvResizeWindow -win $_nWave1 0 23 1920 1017
